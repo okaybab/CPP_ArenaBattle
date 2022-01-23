264b3bb23c98979dd16f15a2e12defdf918ab24d
okaybab <oki8370@naver.com>
2022-01-24 08:15:41 +0900

    chap 3

---
+++ Source/CPP_ArenaBattle/CPP_ArenaBattle.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CPP_ArenaBattle.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(CPP_ArenaBattle);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_ArenaBattle, "CPP_ArenaBattle" );
